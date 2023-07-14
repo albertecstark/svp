e2ec5de4b34ab02dd2228a2bb772b2e9ad257c4b
Albert Stark <albertecstark@gmail.com>
2023-07-14 21:32:23 +0100

    good bit of progress

--- main.py
+++ main.py
@@ -1,39 +1,122 @@
 import math
 import gsw
 import numpy as np
-#import matplotlib as plt
+import matplotlib.pyplot as plt
+import csv
 
-#TODO: find dataset of pressure, salinity and temperature variation with depth (5 deeps ??? where?)
 #TODO: lookup the oriinal documentation of the gsw library (TEOS-10) and find the actual diff eq used (solve general case?)
 
+#dataset is for mariana trench, from https://www.youtube.com/watch?v=-tGusgPTSm4, pulled from dropbox in description
+lat = 11
+long = 142
 
-p = [1,2,3,4,5]
-t = [6,5,4,3,2]
-s = [2,8,2,2,2]
+#region setup and handelling
+#read data into arrays of floats
+filename = open('Flere_03Mar_1_2021.csv', 'r')
+file = csv.DictReader(filename)
 
-def check_in(pr, temp, sal): 
-    if(len(pr)!=len(temp)!=len(sal)):
-        print("mismatched input size")
+pressure = []
+conductivity = []
+temperature = []
+density = []
+speeds_from_dataset = []
+
+for col in file:
+    pressure.append(col['Pressure'])
+    conductivity.append(col[' Conductivity'])
+    temperature.append(col[' Temperature'])
+    density.append(col[' Density'])
+    speeds_from_dataset.append(col[' Sound_Speed'])
+
+pressure = [float(x) for x in pressure]
+conductivity = [float(x) for x in conductivity]
+temperature = [float(x) for x in temperature]
+density = [float(x) for x in density]
+speeds_from_dataset = [float(x) for x in speeds_from_dataset]
+
+#use this method so only have to return max index no any second derivative malarkey
+#also no military submarine goes beneath 1500m, except for crush depth on some soviet ones from the cold war
+#this cutoff gives us ~3700m
+data_len = 500
+
+pressure = pressure[:data_len]
+conductivity = conductivity[:data_len]
+temperature = temperature[:data_len]
+density = density[:data_len]
+speeds_from_dataset = speeds_from_dataset[:data_len]
+
+#defining checks on input size
+def check_in(a,b,c,d,e):
+    if(len(a)!=len(b)!=len(c)!=len(d)!=len(e)):
+        print("! mismatched input size")
         exit(0)
 
-def print_sound():
-    check_in(p,t,s)
-    speed = gsw.sound_speed(s,t,p)
-    LD = max((v ,i) for i, v in enumerate(speed))[1]
-    if (LD < 60):
+#generating depth list
+depth = []
+depth =  -1 * gsw.z_from_p(pressure, lat)
+
+#defining sound speed function
+def sound_speed_deriv(conductivity, temperature, pressure, lat, long):
+    practical_salinity = gsw.SP_from_C(conductivity, temperature, pressure)
+    absolute_salinity = gsw.SA_from_SP(practical_salinity, pressure, long, lat)
+    sound_speed_deriv = gsw.sound_speed(absolute_salinity, temperature, pressure)
+    return sound_speed_deriv
+
+speeds_calc = sound_speed_deriv(conductivity, temperature, pressure, lat, long)
+#endregion
+
+#region calculations on data
+#TODO: implement proper second derivative implementation
+def print_properties_data():
+    BD = 0
+    m_index = speeds_from_dataset.index(max(speeds_from_dataset))
+    LD = depth[m_index]
+    if LD < 60 :
         BD = 17 * math.sqrt(LD)
     else:
         BD = LD + 60
-    print(speed)
-    print(f"The layer is at{LD} and best depth occurs at {BD}")
+    print(f"LD = {LD} and BD = {BD} for the given data")
 
+#make this work later figure out (AttributeError: 'numpy.ndarray' object has no attribute 'index')
+def print_properties_calc():
+    #LD  = list(depth).index(speeds_calc.index(max(speeds_calc)))
+    BD = 0
+    #print(f"LD = {LD} and BD = {BD} for the derived data")
+#endregion
 
-def gen_paths():
-    print("this is in TODO")
+#region image generation
+def gen_img_svps():
+    #the calculated and given data plotted
+    fig, ax = plt.subplots()
+    x1 = depth
+    y11 = speeds_calc
+    y12 = speeds_from_dataset
+    ax.plot(x1,y11, "-b", label="speeds as derived from this program")
+    ax.plot(x1,y12, "-r", label="speeds directly from dataset")
+    ax.legend()
+    plt.savefig('svp.png')
 
+def gen_img_deviations():
+    #deviation between the calculated and given data
+    fig, ax = plt.subplots()
+    x = depth
+    y = speeds_from_dataset - speeds_calc
+    ax.plot(x, y, "-r", label = "Deviation in calculated and given values")
+    plt.savefig('deviation.png')
+
+def gen_img_propagation_paths():
+    #propagation paths
+    #TODO propagation paths
+    fig, ax = plt.subplots()
+    plt.savefig('paths.png')
+#endregion
 
 def main():
-    print_sound()
+    check_in(pressure, conductivity, temperature, density, speeds_from_dataset)
+    print_properties_calc()
+    print_properties_data()
+    gen_img_svps()
+    gen_img_deviations()
 
 if __name__ == "__main__" :
     main()
